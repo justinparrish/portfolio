70de1f887b8cd43e7b90e2bf24850f1e1b691776
justinparrish7@gmail.com <justinparrish7@gmail.com>
2019-09-30 23:09:46 -0400

    added project cards

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react'
-import { Tabs, Tab } from 'react-mdl'
+import React, { Component } from 'react'
+import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardText, CardActions, Button, CardMenu, IconButton } from 'react-mdl'
 
 export default class Projects extends Component {
     state = {
@@ -7,40 +7,56 @@ export default class Projects extends Component {
     }
 
     toggleCategories = () => {
-        if(this.state.activeTab === 0) {
+        if (this.state.activeTab === 0) {
             return (
-                <div>
-                    <h1>Front End Projects</h1>
-                </div>
+                <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                    <CardTitle style={{color: '#fff', height: '170px', background: 'url(images/save_chester.png) center / cover'}}>
+                        Save Chester!
+                    </CardTitle>
+                    <CardText>
+                        Created with HTML5/CSS3 and VanillaJs.
+                    </CardText>
+                    <CardActions border style={{display: 'flex', justifyContent: 'space-around'}}>
+                        <Button colored href='https://github.com/justinparrish/project-one'>GitHub</Button>
+                        <Button colored href='https://justinparrish.github.io/project-one/'>Deployed App</Button>
+                    </CardActions>
+                    <CardMenu style={{color: '#fff'}}>
+                        <IconButton name='share'/>
+                    </CardMenu>
+                </Card>
             )
-        } else if(this.state.activeTab === 1) {
+        } else if (this.state.activeTab === 1) {
             return (
                 <div>
                     <h1>Back End Projects</h1>
                 </div>
             )
-        } else if(this.state.activeTab === 2) {
+        } else if (this.state.activeTab === 2) {
             return (
                 <div>
                     <h1>Full Stack Projects</h1>
                 </div>
             )
-        } 
+        }
     }
-    
+
     render() {
         return (
             <div className='category-tabs'>
-                <Tabs activeTab={this.state.activeTab} 
-                    onChange={(tabId) => this.setState({ activeTab: tabId})} ripple>
-                        <Tab>Front End</Tab>
-                        <Tab>Back End</Tab>
-                        <Tab>Full Stack</Tab>
+                <Tabs activeTab={this.state.activeTab}
+                    onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
+                    <Tab>Front End</Tab>
+                    <Tab>Back End</Tab>
+                    <Tab>Full Stack</Tab>
                 </Tabs>
                 <section className='project-grid'>
-                {this.toggleCategories()} 
+                    <Grid className='projects-grid'>
+                        <Cell col={12}>
+                            <div className='content'>{this.toggleCategories()}</div>
+                        </Cell>
+                    </Grid>
                 </section>
-            </div>
+            </div >
         )
     }
 }
