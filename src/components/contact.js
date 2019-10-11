b494256ccf7846320c17efd056c1f19f6a86ac6d
justinparrish7@gmail.com <justinparrish7@gmail.com>
2019-10-11 18:54:21 -0400

    update

--- src/components/contact.js
+++ src/components/contact.js
@@ -14,7 +14,7 @@ export default class ContactMe extends Component {
                     <p style={{ width: '75%', margin: 'auto', paddingTop: '1em' }}>
                         Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
                         industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
-                        and scrambled it to make a type specimen book. It has survived not only five centuries
+                        and scrambled it to make a type specimen book. It has survived not only five centuries.
                     </p>
                 </Cell>
                 <Cell col={6}>
