0866b85ea52a65cbd5af70f5bca45fdad116f2f5
justinparrish7@gmail.com <justinparrish7@gmail.com>
2019-10-17 14:21:21 -0400

    complete resume page

--- src/components/resume.js
+++ src/components/resume.js
@@ -42,17 +42,20 @@ export default class Resume extends Component {
                             startYear='July 2019'
                             endYear='October 2019'
                             schoolName='General Assembly'
-                            schoolDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-                            industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
-                            and scrambled it to make a type specimen book. It has survived not only five centuries"
+                            schoolDescription="As an alumni of this school, I was taught all the concepts of
+                            the software engineering world. From computer science to coding four stunning projects
+                            with four different objectives, this school has made me into a flexible software engineer.
+                            Our focuses here was Javascript, React, and Full Stack Development with framework use.
+                            We also learned and coded in Python making the students bilingual coders."
                         />
                         <Education 
                             startYear='August 2014'
                             endYear='May 2018'
                             schoolName='Martin Luther King Jr. High School'
-                            schoolDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-                            industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
-                            and scrambled it to make a type specimen book. It has survived not only five centuries"
+                            schoolDescription="As a high school alumni, I was taught the basics of life from schooling
+                            and decision making. There were multiple career paths to choose from, so I was placed into
+                            a software engineer's path, in which I chose. Along side of my career path I was placed in
+                            AP courses graduating with a 3.1 GPA."
                         />
                         <hr style={{borderTop: '3px solid #0083B0'}} />
                         
@@ -61,9 +64,11 @@ export default class Resume extends Component {
                             startYear='July 2019'
                             endYear='October 2019'
                             jobName='General Assembly'
-                            jobDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-                            industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
-                            and scrambled it to make a type specimen book. It has survived not only five centuries"
+                            jobDescription="While being at GA, I worked on many React projects and activies that also included
+                            databases. I dealt with MongoDB, and SQLite databases. A few projects required CRUD implementation,
+                            with the use of an Agile Development process. We used various framework and Javascript libraries, 
+                            but many we used VanilllaJs and some jQuery. Some Python code was used for Full Stack applications,
+                            mainly when Django was involved, but ExpressJs and NodeJs was normally of choice for most applications."
                         />
                         <hr style={{ borderTop: '3px solid #0083B0'}} />
 
