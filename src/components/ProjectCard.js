67fddf230a9a2eff98bbd9f308a476fe69a19cdf
justinparrish7@gmail.com <justinparrish7@gmail.com>
2019-10-17 12:13:00 -0400

    sending props to project component

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -4,16 +4,16 @@ import { Card, CardTitle, CardText, CardActions, Button, CardMenu, IconButton } 
 export default class ProjectCard extends Component {
 
     render = () => (
-        <Card shadow={5} style={{ width: '500px', margin: 'auto' }}>
-        <CardTitle style={{ color: '#fff', height: '270px', background: 'url(images/motorboard.png) center / cover' }}>
-            MotorBoard
+        <Card shadow={5} style={{ width: '500px', margin: 'auto', marginBottom: '50px' }} className='card'>
+        <CardTitle style={{ color: '#fff', height: '270px', background: `${this.props.image}` }}>
+            {this.props.title}
     </CardTitle>
         <CardText>
-            Created with React and Django.(Frameworks used were AntDesign ,React-MDL)
+            {this.props.text}
     </CardText>
         <CardActions border style={{ display: 'flex', justifyContent: 'space-around' }}>
-            <Button colored href='https://github.com/justinparrish/project-four'>GitHub</Button>
-            <Button colored href='https://justinparrish.github.io/project-one/'>Deployed App</Button>
+            <Button colored href={this.props.github}>GitHub</Button>
+            <Button colored href={this.props.deployedApp}>Deployed App</Button>
         </CardActions>
         <CardMenu style={{ color: '#fff' }}>
             <IconButton name='share' />
